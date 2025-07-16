9375e81c69158c9372be99ddc6f20bab595ef4f2
islam <islamamara062@gmail.com>
2025-07-16 23:52:19 +0000

    I added the main bg

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 
 const App = () => {
   return (
-    <div>
+    <div className="min-h-screen flex justify-center items-center bg-stone-900">
+      <h1>Test</h1>
       
     </div>
-  )
+  );
 }
 
 export default App
