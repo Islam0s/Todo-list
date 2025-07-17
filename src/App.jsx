2618f002f882b06778aa36b1d7d67ecd3b329d71
islam <islamamara062@gmail.com>
2025-07-17 09:47:02 +0000

    Tasks input Section UI is completed.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,14 @@ const App = () => {
   return (
     <div className="min-h-screen flex justify-center items-center bg-stone-900">
       <div className="bg-white p-16 rounded-lg">
-        
+        <div className="flex gap-6">
+          <input 
+            type="text" 
+            placeholder="Add a new Task."
+            className="p-2 rounded-lg"
+          />
+          <button className="bg-amber-400 text-white p-2 rounded-lg hover:bg-amber-300">Add Task</button>
+        </div>
       </div>
       
     </div>
