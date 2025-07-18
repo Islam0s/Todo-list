ddd1ce9b1402bcd64cf965065b238fb9cfd4f39c
islam <islamamara062@gmail.com>
2025-07-18 17:52:16 +0000

    I Completed the UI

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,35 @@
 const App = () => {
   return (
     <div className="min-h-screen flex justify-center items-center bg-stone-900">
-      <div className="bg-white p-16 rounded-lg">
-        <div className="flex gap-6">
-          <input 
-            type="text" 
-            placeholder="Add a new Task."
-            className="p-2 rounded-lg"
-          />
-          <button className="bg-amber-400 text-white p-2 rounded-lg hover:bg-amber-300">Add Task</button>
+      
+      <div className="bg-sky-500 p-16 rounded-lg space-y-10">
+          
+        <div>
+
+          <div className="flex gap-6">
+            <input 
+              type="text" 
+              placeholder="Add a new Task."
+              className="bg-white p-2 rounded-lg"
+            />
+            <button className="bg-green-500 text-white p-2 rounded-lg hover:bg-amber-300">Add Task</button>
+          </div>
+
+      
         </div>
+
+        <div className="text-white">
+          <ol>
+            <div className="flex gap-6 items-center justify-center">
+
+              <li className="text-lg bg-sky-700 py-2 px-5 rounded-lg">Eat pizza wiht my wife.</li>
+              <button className="bg-green-500 p-2 rounded-lg">Done</button>
+              <button className="bg-red-400 p-2 rounded-lg">Delete</button>
+ 
+            </div>
+          </ol>
+        </div>
+
       </div>
       
     </div>
