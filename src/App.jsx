e066ee3b98926509d470882671f881c1b70da091
islam <islamamara062@gmail.com>
2025-07-18 22:34:28 +0000

    a small improvment in the UI

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ const App = () => {
   return (
     <div className="min-h-screen flex justify-center items-center bg-stone-900">
       
-      <div className="bg-sky-500 p-16 rounded-lg space-y-10">
+      <div className="bg-sky-500 p-14 rounded-lg space-y-10">
           
         <div>
 
@@ -20,12 +20,24 @@ const App = () => {
         </div>
 
         <div className="text-white">
-          <ol>
-            <div className="flex gap-6 items-center justify-center">
+          <ol className="space-y-6">
+            <div className="gap-6 flex items-center">
 
-              <li className="text-lg bg-sky-700 py-2 px-5 rounded-lg">Eat pizza wiht my wife.</li>
-              <button className="bg-green-500 p-2 rounded-lg">Done</button>
-              <button className="bg-red-400 p-2 rounded-lg">Delete</button>
+              <li className="text-lg bg-sky-700 py-2 px-5 rounded-lg">Eat pizza with my wife</li>
+              <div className="justify-center space-x-4">
+                <button className="bg-green-500 p-2 rounded-lg">Done</button>
+                <button className="bg-red-400 p-2 rounded-lg">Delete</button>
+              </div>
+ 
+            </div>
+
+            <div className="gap-6 flex items-center">
+
+              <li className="text-lg bg-sky-700 py-2 px-5 rounded-lg">Learn react js</li>
+              <div className="justify-end space-x-4">
+                <button className="bg-green-500 p-2 rounded-lg">Done</button>
+                <button className="bg-red-400 p-2 rounded-lg">Delete</button>
+              </div>
  
             </div>
           </ol>
